270058900496d16aa00c3640f15571f763278046
Villiam Riegler <ville@riegler.se>
2023-03-25 17:48:10 +0100

    Added vim-fuitive

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -79,6 +79,7 @@ return require('packer').startup(function(use)
 		end
 	}
 
+	use "tpope/vim-fugitive"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
